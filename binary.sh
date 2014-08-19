23e4935be192e9eb127bba6db1b4efc22daa4493
Edward A Maxedon Sr <edadma@gmail.com>
2014-08-19 13:01:32 -0400

    fix project path

--- binary.sh
+++ binary.sh
@@ -1,1 +1,1 @@
-rsync -azP -e ssh --delete /home/ed/projects/CALab/target/scala-2.11/calab-*.jar ed@hyperreal.ca:/var/www/hyperreal.ca/releases
+rsync -azP -e ssh --delete /home/ed/projects/calab/target/scala-2.11/calab-*.jar ed@hyperreal.ca:/var/www/hyperreal.ca/releases
